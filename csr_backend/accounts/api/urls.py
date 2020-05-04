9afff04a033cba7a0b1e338b50271fd1401753d4
nightwarrior-xxx <amanv1999@gmail.com>
2020-05-05 03:48:33 +0530

    Added logout url

--- csr_backend/accounts/api/urls.py
+++ csr_backend/accounts/api/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('auth/register', RegisterAPI.as_view()),
     path('auth/login', LoginAPI.as_view()),
     path('auth/user', UserAPI.as_view()),
+    path('auth/logout', knox_views.LogoutView.as_view(), name='logout')
 ]
